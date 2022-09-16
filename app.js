8aaafd996ea6595567de444b7a6df880d774dcbd
WebAzizbek <azizbekweb3365@gmail.com>
2022-09-16 16:44:01 +0500

    payapi

---
+++ app.js
@@ -0,0 +1,27 @@
+// LOADER
+
+var loader = document.querySelector(".loader")
+
+window.addEventListener("load", vanish);
+
+function vanish(){
+    loader.classList.add("disppear");
+}
+
+// REMOVE
+const barsBtn = document.querySelector(".bars-btn")
+const closeBtn = document.querySelector(".right-nav-btn")
+const rightNav = document.querySelector(".right-nav")
+
+
+
+
+barsBtn.addEventListener("click", function() {
+    rightNav.classList.add("toggle")
+    rightNav.classList.remove("remove")
+})
+
+closeBtn.addEventListener("click", function() {
+    rightNav.classList.remove("toggle")
+    rightNav.classList.add("remove")
+})
